03c886aeb3019b0ccbaf2b89e98d063ee05607a7
KamiMonteiro <monteiro.isabeldossantos@gmail.com>
2025-05-03 22:25:33 -0300

    Alteracao de classe global scanner para classe local Teclado.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 import java.util.ArrayList;
-//import java.util.Scanner;
+//import java.util.Scanner; //apenas caso de usar classe gloal
 
 public class Main {
     public static void main(String[] args) {
