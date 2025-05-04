346e68a73d50a9510355d79a1c364b6edf20b61d
KamiMonteiro <monteiro.isabeldossantos@gmail.com>
2025-05-03 21:53:17 -0300

    Alteracao de classe global scanner para classe local Teclado.

---
+++ src/Doce.java
@@ -0,0 +1,17 @@
+public class Doce {
+    //String nome = "Gardem"; //declaraçao correta
+    double preco; //declaraçao correta
+    String nomeDoce; //declaraçao correta
+
+
+    void mostrarInfoDoce() {
+        System.out.println("O nome do doce: " + nomeDoce);
+        System.out.println("O valor do doce é: " + preco);
+    }
+
+    /*
+    void mostrarNomeLoja() {
+        System.out.println("A loja se chama " + nome);
+    }*/
+
+}
---
+++ src/Main.java
@@ -0,0 +1,64 @@
+
+import java.util.ArrayList;
+//import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Doce> listaDeDoces = new ArrayList<>(); //Lista que só aceita objetos da classe Doce
+
+
+        Doce doce1  = new Doce ();
+        doce1 .nomeDoce = "Bem Casado";
+        doce1 .preco = 3.45;
+
+        Doce doce2 = new Doce();
+        doce2.nomeDoce = "Beijinho";
+        doce2.preco = 2.00;
+
+        Doce doce3 = new Doce();
+        doce3.nomeDoce = "Brigadeiro";
+        doce3.preco = 1.55;
+
+        Doce doce4 = new Doce();
+        doce4.nomeDoce = "Coxinha de Morango";
+        doce4.preco = 8.50;
+
+        //Comando que coloca um doce dentro da lista
+        listaDeDoces.add(doce1);
+        listaDeDoces.add(doce2);
+        listaDeDoces.add(doce3);
+        listaDeDoces.add(doce4);
+
+
+        //Scanner para ler entrada do usuário
+
+        String nomeBuscado = Teclado.lerString("Digite o dooce que procura: ");
+
+        boolean encontrado = false;
+
+        for (Doce doceAtual : listaDeDoces) {
+            if (doceAtual.nomeDoce.equalsIgnoreCase(nomeBuscado)){
+                System.out.println("✨ Doce encontrado: ");
+                doceAtual.mostrarInfoDoce();
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("😔 Doce não encontrado");
+        }
+
+/*
+        System.out.println(" 📃 Lista de Doces: ");
+        //Pega cada doce da lista um por um e mostra
+        for (Doce d : listaDeDoces) {
+            //d.mostrarNomeLoja();
+            d.mostrarInfoDoce();
+            System.out.println("________*____________");
+
+        }
+
+ */
+    }
+}
